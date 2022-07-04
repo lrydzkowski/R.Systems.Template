4280a9f708eb7c7c09715fa0ad8cfadfb909118f
Łukasz Rydzkowski <lukasz.rydzkowski@gmail.com>
2022-07-04 16:33:50 +0200

    feat: add NLog

--- R.Systems.Template.WebApi/Program.cs
+++ R.Systems.Template.WebApi/Program.cs
@@ -1,3 +1,5 @@
+using NLog;
+using NLog.Web;
 using R.Systems.Template.Core;
 
 namespace R.Systems.Template.WebApi;
@@ -6,21 +8,39 @@ public class Program
 {
     public static void Main(string[] args)
     {
-        var builder = WebApplication.CreateBuilder(args);
-        builder.Services.ConfigureServices();
-        builder.Services.ConfigureCoreServices();
+        var logger = NLog.LogManager.Setup().LoadConfigurationFromAppSettings().GetCurrentClassLogger();
+        logger.Debug("init main");
 
-        var app = builder.Build();
-
-        app.UseSwagger();
-        if (app.Environment.IsDevelopment())
+        try
         {
-            app.UseSwaggerUI();
-        }
-        app.UseHttpsRedirection();
-        app.UseAuthorization();
-        app.MapControllers();
+            var builder = WebApplication.CreateBuilder(args);
+            builder.Services.ConfigureServices();
+            builder.Services.ConfigureCoreServices();
 
-        app.Run();
+            builder.Logging.ClearProviders();
+            builder.Host.UseNLog();
+
+            var app = builder.Build();
+
+            app.UseSwagger();
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwaggerUI();
+            }
+            app.UseHttpsRedirection();
+            app.UseAuthorization();
+            app.MapControllers();
+
+            app.Run();
+        }
+        catch (Exception exception)
+        {
+            logger.Error(exception, "Stopped program because of exception");
+            throw;
+        }
+        finally
+        {
+            NLog.LogManager.Shutdown();
+        }
     }
 }
