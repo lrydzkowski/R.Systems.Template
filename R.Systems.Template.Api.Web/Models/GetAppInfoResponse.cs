da45e5c65fefed78b2a7ad61ea480df3da1db9d0
Łukasz Rydzkowski <lukasz.rydzkowski@gmail.com>
2023-06-13 19:54:16 +0200

    feat: remove temporary properties

--- R.Systems.Template.Api.Web/Models/GetAppInfoResponse.cs
+++ R.Systems.Template.Api.Web/Models/GetAppInfoResponse.cs
@@ -5,8 +5,4 @@ public record GetAppInfoResponse
     public string AppName { get; init; } = "";
 
     public string AppVersion { get; init; } = "";
-
-    public string Test { get; init; } = ":)";
-
-    public string Test2 { get; init; } = ":) 2";
 }
