540dbfb5131061714758fa00701ba1ebc89d8f02
Łukasz Rydzkowski <lukasz.rydzkowski@gmail.com>
2023-10-17 21:01:53 +0200

    feat: clear logging providers in tests

--- R.Systems.Template.Tests.Api.Web.Integration/Common/WebApplication/WebApiFactory.cs
+++ R.Systems.Template.Tests.Api.Web.Integration/Common/WebApplication/WebApiFactory.cs
@@ -1,11 +1,10 @@
-﻿using System.Text.Json;
-using FluentAssertions;
-using FluentAssertions.Equivalency.Steps;
+﻿using FluentAssertions;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc.Testing;
 using Microsoft.AspNetCore.TestHost;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 using R.Systems.Template.Api.Web;
 using R.Systems.Template.Infrastructure.Db.Common.Options;
 using R.Systems.Template.Tests.Api.Web.Integration.Common.Assertion;
@@ -56,6 +55,9 @@ public class WebApiFactory : WebApplicationFactory<Program>, IAsyncLifetime
 
     protected override void ConfigureWebHost(IWebHostBuilder builder)
     {
+        builder.ConfigureLogging(
+            x => x.ClearProviders()
+        );
         builder.ConfigureAppConfiguration(
             (_, configBuilder) =>
             {
